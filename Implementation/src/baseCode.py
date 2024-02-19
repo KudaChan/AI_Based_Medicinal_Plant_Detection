50b13af81885350b59ec1bad31aa9ea467b66140
KudaChan <kumarnchandan05@gmail.com>
2024-02-19 20:48:51 +0530

    commit codes

--- Implementation/src/baseCode.py
+++ Implementation/src/baseCode.py
@@ -1,35 +1,8 @@
 import os
 import cv2
-from tqdm import tqdm
 
 
 from myCustomPackages import pp1_Resizing
-
-
-"""_summary_
-    Image Preprocessing:
-"""
-class ImageResizing:
-    def __init__(self, input_dir, output_dir, resizer):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
-        self.resizer = resizer
-
-    def resize_images(self):
-        files = list(os.walk(self.input_dir))
-        for root, dirs, files in tqdm(os.walk(self.input_dir), total=len(files), desc="Resizing images"):
-            for file in files:
-                if file.endswith(('.png', '.jpg', '.jpeg')):
-                    input_path = os.path.join(root, file)
-                    output_dir = root.replace(self.input_dir, self.output_dir)
-                    os.makedirs(output_dir, exist_ok=True)
-                    output_path = os.path.join(output_dir, file)
-                    resized_image = self.resizer.resize(input_path)
-                    cv2.imwrite(output_path, resized_image)
-
-class ResizerSelector:
-    def __init__(self, target_size):
-        self.factory = pp1_Resizing.ImageResizerFactory(target_size)
 
 
 
@@ -42,10 +15,8 @@ if __name__ == "__main__":
 
     # Create a resizer that uses Bicubic interpolation
     resizeTechnique = ['Bilinear', 'BiCubic', 'Lanczos']
-    factory = pp1_Resizing.ImageResizerFactory((224, 224))
+    target_size = (224, 224)
     for technique in resizeTechnique:
-        resizer = factory.resize[technique]
-
         # Create an instance of ImageResizing and use it to resize all images in the input directory
-        image_resizing = ImageResizing(input_dir, f"{output_dir}/{technique}", resizer)
-        image_resizing.resize_images()
+        image_resizing = pp1_Resizing.ImageResizing(input_dir, f"{output_dir}/{technique}", target_size)
+        image_resizing.resize_images(technique)
--- Implementation/src/myCustomPackages/pp1_Resizing.py
+++ Implementation/src/myCustomPackages/pp1_Resizing.py
@@ -5,10 +5,13 @@ Classes:
 - BilinearInp: Implements the Bilinear interpolation technique to resize an image.
 - BiCubicInp: Implements the Bicubic interpolation technique to resize an image.
 - LanczosResampling: Implements the Lanczos Resampling technique to resize an image.
+- ImageResizerFactory: Creates objects of different interpolation techniques to resize an image.
+- ImageResizing: Resize Image in a directory.
 """
 
 import os
 import cv2
+from tqdm import tqdm
 
 class BilinearInp:
     """
@@ -67,3 +70,30 @@ class ImageResizerFactory:
             return self.resizers[interpolation_technique].resize(image_path)
         else:
             raise ValueError("Invalid interpolation technique")
+
+
+
+class ImageResizing:
+    """
+    This class is used to resize images in a directory using different interpolation techniques.
+    """
+    def __init__(self, input_dir, output_dir, target_size):
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        self.resizer_factory = ImageResizerFactory(target_size)
+
+    def resize_images(self, technique):
+        files = list(os.walk(self.input_dir))
+        for root, dirs, files in tqdm(os.walk(self.input_dir), total=len(files), desc="Resizing images"):
+            for file in files:
+                if file.endswith(('.png', '.jpg', '.jpeg')):
+                    input_path = os.path.join(root, file)
+                    output_dir = root.replace(self.input_dir, self.output_dir)
+                    os.makedirs(output_dir, exist_ok=True)
+                    output_path = os.path.join(output_dir, file)
+                    print(f"Processing Image : {input_path}")
+                    try:
+                        resized_image = self.resizer_factory.resize(input_path, technique)
+                        cv2.imwrite(output_path, resized_image)
+                    except Exception as e:
+                        print(f"Error processing file {input_path}: {e}")
